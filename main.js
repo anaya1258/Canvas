507d3ad3f5d31a8c88c3dbc54ed315f919c6b999
anaya1258 <83693191+anaya1258@users.noreply.github.com>
2021-11-30 13:56:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+var mouseEvent="emtpy";
+var last_position_of_x,last_position_of_y
+color="purple";
+width_of_line=5;
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    mouseEvent = "mouseDown";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+
+function my_mouseleave(e)
+{
+    mouseEvent = "mouseLeave";
+}
+
+canvas.addEventListener("mouseup", my_mouseup);
+
+function my_mouseup(e)
+{
+    mouseEvent = "mouseUp";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
+function my_mousemove(e)
+{
+current_position_of_x = e.clientX - canvas.offsetLeft;
+current_position_of_y = e.clientY - canvas.offsetTop; 
+
+if (mouseEvent == "mouseDown") {
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = width_of_line;
+
+console.log("Last position of x and y coordinates = ");
+console.log("x = "+ last_position_of_x + "y = "+ last_position_of_y);
+ctx.moveTo(last_position_of_x, last_position_of_y);
+
+console.log("Current position of x and y coordinates = ");
+console.log("x = "+ current_position_of_x + "y = "+ current_position_of_y);
+ctx.lineTo(current_position_of_x, current_position_of_y);
+ctx.stroke();
+}
+
+last_position_of_x = current_position_of_x;
+last_position_of_y = current_position_of_y;
+}
+
+//MoveTo is the starting point
+//LineTo is the ending point
+//Stroke will give the color 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
